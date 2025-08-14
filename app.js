5d07e4c8382a5089073df818dab8217ca0f84848
RXnZ11 <ronaldlemosv08@gmail.com>
2025-08-14 01:31:18 -0500

    Versión preliminar del juego "sortear amigo"
    En esta versión del código se llegó hasta la sección de mostrar la lista de amigos. 
    Proximamente se actualizará para implementar nuevas funciones.

---
+++ app.js
@@ -0,0 +1,28 @@
+let amigos = []; //almacenar el nombre los amigos
+
+console.log(amigos) //muestra en la consola que los nombres están siendo registrados en el array amigos
+function agregarAmigo() {
+    let nombreIngresadoUsuario = document.getElementById("amigo").value.trim();
+    if(nombreIngresadoUsuario === "") {
+        alert("El campo está vacío, por favor ingrese un nombre.");
+} else {
+    if (nombreIngresadoUsuario !== "") { //si el campo de texto no está vacío se aplica en .push y se agrega el nombre al array.
+        amigos.push(nombreIngresadoUsuario)
+        document.getElementById("amigo").value = "";
+    }
+}
+}
+
+function mostrarListaAmigos () {
+    let lista = document.getElementById("listaAmigos") ;
+    lista.innerHTML = "";
+
+    for(let i = 0 ; i < amigos.length ; i++) {
+        lista.innerHTML += `<li> ${amigos[i]}</li>` ;
+    }
+}
+        
+    
+
+
+
